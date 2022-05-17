f9b65b6c1474b4d52eb6c163949aca0edac89a03
Juan Manuel Aragon Paz <jmanuel@thinkanddev.com>
2022-05-17 01:06:43 -0300

    Add distance and handicap system

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 let container = document.getElementById("titulo")
 container.innerHTML = "<h2>Bienvenido a Tarjeta de Golf Virtual</h2>"
-// let file = ''
+let handicap=''
+let hcp = document.getElementById("handicap")
+hcp.addEventListener('change', () =>{
+    handicap = parseInt(hcp.value);
+    return handicap
+})
+
 function modificarPlantillaHoles() {
     const contenedor = document.getElementById("name");
     contenedor.innerHTML = `<div>
@@ -8,6 +14,7 @@ function modificarPlantillaHoles() {
                                 <option selected="Elija un campo">Elija un Campo</option>
                                 <option value="cancha.json">Club Hípico de City Bell</option>
                                 <option value="cancha2.json">UPCN</option>
+                                <option value="cancha3.json">Augusta National</option>
                                 </select>
                             </div>`
 }
@@ -18,12 +25,10 @@ let tabla=''
 let parDeCampo=0
 selection.addEventListener('change', () => {
     file = selection.options[selection.selectedIndex].value;
-    console.log(file)
     return file
 })
 function waitForElement(){
     if(typeof file !== "undefined"){
-        console.log(file)
         tabla = document.querySelector('#lista');
         cargarCancha();
         let puntos = document.querySelector('#puntos');
@@ -32,7 +37,6 @@ function waitForElement(){
         startR.addEventListener('click', borrarRonda);
     }
     else{
-        // console.log("Elija un campo")
         setTimeout(waitForElement, 250);
     }
 }
@@ -43,12 +47,12 @@ function cargarCancha() {
         .then(respuesta => respuesta.json())
         .then(field => {
             const thead = document.createElement('tr')
-            thead.innerHTML = `<th>Hoyo</th> <th>Par</th><th>Score</th>`
+            thead.innerHTML = `<th>Hoyo</th> <th>Par</th> <th>Yardas</th> <th>Hcp</th> <th>Score</th>`
             tabla.appendChild(thead);
             field.forEach(Hoyo => {
                 const row = document.createElement('tr');
                 row.innerHTML += `
-                    <td>${Hoyo.Hoyo}</td> <td>${Hoyo.Par}</td> <td><input class="form-control score" type="number" placeholder="Score" aria-label="Score"></td>
+                    <td>${Hoyo.Hoyo}</td> <td>${Hoyo.Par}</td> <td>${Hoyo.Yd}</td> <td>${Hoyo.Hcp}</td> <td><input class="form-control score" type="number" placeholder="Score" aria-label="Score"></td>
                 `;
                 tabla.appendChild(row);
                 parDeCampo += Hoyo.Par
@@ -63,7 +67,9 @@ function storeScore() {
     for (let score of scorer) {
         total = parseInt(score.value) + total
     }
+    total2= total - parseInt(handicap)
     even = total - parDeCampo;
+    neto = total - parDeCampo - parseInt(handicap);
     if (even == 0) {
         even = 'E'
     } else if (even < 0) {
@@ -71,19 +77,24 @@ function storeScore() {
     } else {
         even = `+${even}`
     }
+    if (neto == 0) {
+        neto = 'E'
+    } else if (neto < 0) {
+        neto = `${neto}`
+    } else {
+        neto = `+${neto}`
+    }
     if (isNaN(total)) {
         mostrarAlertaErr()
-        document.getElementById("final").innerHTML = `<h3 class="final">Error, verifique la carga correcta del score en todos los hoyos.</h3>`
     } else {
         mostrarAlertaOk()
-        document.getElementById("final").innerHTML = `<h3 class="final">Su score final es de ${total} (${even})</h3>`
     }
 }
 
 function mostrarAlertaOk() {
     Swal.fire({
         title: 'Score',
-        text: `Su Score Final es de ${total} (${even})`,
+        text: `Su Score Final (Gross) es de ${total} (${even}). Su Score Neto (con handicap) es de ${total2} (${neto})`,
         icon: 'success',
         confirmButtonText: 'Ok',
     })
@@ -102,6 +113,7 @@ function borrarRonda() {
     const scorer = document.getElementsByClassName("score")
     for (let score of scorer) {
         score.value = ''
+        handicap=''
     }
     location.reload()
 }
